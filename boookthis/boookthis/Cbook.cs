8a17b96e3719cd94bff8276e80324f9c9a23836c
perforator-coder <nicita.paschkovsky@mail.ru>
2025-10-21 15:16:11 +0300

    help

---
+++ boookthis/boookthis/Cbook.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boookthis
+{
+    internal class Cbook
+    {
+        private int pages;
+        private string name_book;
+       // private bool er_name;
+        private bool er_page;
+
+        //конструкторы
+        public Cbook()
+        {
+            this.pages = 1;
+            this.name_book = "";
+        }
+        public Cbook(string name)
+        { 
+            this.name_book = name;
+            this.pages = 100;
+        }
+        public Cbook(string name,int pages)
+        {
+            if (pages < 1 || pages > 5000)
+            {
+                Console.WriteLine("Ошибка: не в диапозоне.");
+                er_page = true;
+            }
+            else
+            {
+            this.pages = pages;
+            }
+            this.name_book = name ;
+        }
+        
+        public string Namecheck
+        {
+            get { return name_book; }
+            set
+            { 
+             name_book = value ;
+            }
+        }
+        public int Pages
+        {
+            get { return pages; }
+            set
+            {
+                if (value < 1 || value > 5000)
+                {
+                    Console.WriteLine("Ошибка: не в диапозоне.");
+                    er_page = true;
+                }
+                else
+                {
+                    pages = value;
+                    er_page = false;
+                }
+            }
+        }
+
+        // название
+
+        public void Read()
+        {
+            if (er_page == false)
+            {
+                Console.WriteLine($"Читаю книгу {name_book}, с {pages} страниц.");
+            }
+            
+        }
+    }
+}
---
+++ boookthis/boookthis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boookthis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cbook book = new Cbook("test");
+            book.Read();
+            Cbook book_1 = new Cbook("test",0);
+            book_1.Read();
+            Cbook book_2 = new Cbook();
+            book_2.Read();
+        }
+    }
+}
